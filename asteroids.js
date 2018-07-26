56602ad88693c159002b6d6f70d7407e0e687842
Prakhar Baveja <prakhar.baveja@gmail.com>
2018-07-26 21:16:24 +0530

    setInterval to update drawing of the ship

--- asteroids.js
+++ asteroids.js
@@ -1,7 +1,20 @@
 var Asteroids = function(theGame) {
     this.thecanvas = Asteroids.thecanvas(this, theGame);
     this.player = Asteroids.player(this);
+
+    Asteroids.play(this);
     return this;
+}
+
+Asteroids.play = function(game){
+    var context = game.thecanvas.getContext('2d');
+
+    setInterval(update, 1000/FPS);
+
+    function update() {
+        game.player.draw(context);
+    }
+
 }
 
 Asteroids.thecanvas = function(game, theGame) {
@@ -13,24 +26,28 @@ Asteroids.thecanvas = function(game, theGame) {
 }
 
 Asteroids.player = function(game) {
+
     var pos = [GAME_WIDTH/2,GAME_HEIGHT/2];
     var direction = -Math.PI/2;
-    var context = game.thecanvas.getContext('2d');
     
-    context.translate(pos[0], pos[1]);
-    context.rotate(direction);
+    return {
+        draw: function(context) {
+            context.translate(pos[0], pos[1]);
+            context.rotate(direction);
+            context.beginPath();
 
-    context.beginPath();
+            context.moveTo(10,0);
+            context.lineTo(-5,5);
+            context.lineTo(-5,-5);
+            context.lineTo(10,0);
 
-    context.moveTo(10,0);
-    context.lineTo(-5,5);
-    context.lineTo(-5,-5);
-    context.lineTo(10,0);
-    
-    context.stroke();
+            context.stroke();
+        }
+    }
 }
 
 GAME_WIDTH = 600;
 GAME_HEIGHT = 480;
+FPS = 30; 
 
 window.onload = Asteroids(document.getElementById('theGame'));
